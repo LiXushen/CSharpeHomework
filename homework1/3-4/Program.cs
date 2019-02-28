7d605a24d5db7f94437085563f92ff92bc384182
LiXushen <48084613+LiXushen@users.noreply.github.com>
2019-02-28 20:14:41 +0800

    Add files via upload

---
+++ homework1/3-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int a, b;
+            System.Console.Write("请输入第一个数:");
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            System.Console.Write("请输入第二个数:");
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            Console.WriteLine("积："+(a*b));
+            Console.ReadKey();
+        }
+    }
+}
