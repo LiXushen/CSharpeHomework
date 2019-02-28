1c2aa294af99078820d18f934e86bbeb3b52affe
LiXushen <48084613+LiXushen@users.noreply.github.com>
2019-02-28 21:15:27 +0800

    Add files via upload

---
+++ homework1/4-5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int a = Int32.Parse(s);
+            s = textBox2.Text;
+            int b = Int32.Parse(s);
+            label.Text = a + "*" + b + "=" + a * b;
+        }
+    }
+}
