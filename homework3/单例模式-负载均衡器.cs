159a70e099e19fdab6217a2b695e1f0541e1c25a
LiXushen <48084613+LiXushen@users.noreply.github.com>
2019-03-11 11:17:52 +0800

    Add files via upload

---
+++ homework3/单例模式-负载均衡器.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadBalancer balancer1 = LoadBalancer.getLoadBalancer();
+            LoadBalancer balancer2 = LoadBalancer.getLoadBalancer();
+            LoadBalancer balancer3 = LoadBalancer.getLoadBalancer();
+            LoadBalancer balancer4 = LoadBalancer.getLoadBalancer();
+            if (balancer1 == balancer2 && balancer1 == balancer3 && balancer1 == balancer4) Console.WriteLine("服务器负载均衡性具有唯一性!");
+            balancer1.addServer("Server 1");
+            balancer1.addServer("Server 2");
+            balancer1.addServer("Server 3");
+            balancer1.addServer("Server 4");
+            for(int i = 0; i < 10; i++)
+            {
+                String server = balancer1.getServer();
+                Console.WriteLine("分发请求至服务器："+server);
+            }
+        }
+    }
+    class LoadBalancer
+    {
+        private static LoadBalancer instance = null;
+        private List<string> serverList = null;
+        private static Random random = new Random();
+        private LoadBalancer()
+        {
+            serverList = new List<string>();
+        }
+        public static LoadBalancer getLoadBalancer()
+        {
+            if (instance == null) instance = new LoadBalancer();
+            return instance;
+        }
+        public void addServer(String server)
+        {
+            serverList.Add(server);
+        }
+        public void removeServer(String server)
+        {
+            serverList.Remove(server);
+        }
+        public string getServer()
+        {
+            int i = random.Next(serverList.Count);
+            return serverList.ElementAt(i);
+        }
+    }
+}
---
+++ homework3/策略模式-电影票价打折方案.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieTicket mt = new MovieTicket();
+            double originalPrice = 60.0;
+            mt.setPrice(originalPrice);
+            Console.WriteLine("原始票价为：" + originalPrice);
+
+            Discount discount = getDiscount();
+            mt.setDiscount(discount);
+            double currentPrice=mt.getPrice();
+            Console.WriteLine("折后价为：" + currentPrice);
+
+        }
+        public static Discount getDiscount()
+        {
+            string s;
+            int i;
+            System.Console.Write("请选择(1.学生  2.儿童    3.VIP):");
+            s = Console.ReadLine();
+            i = Int32.Parse(s);
+            switch(i)
+            {
+                case 1:return new StudentDiscount();
+                case 2: return new StudentDiscount();
+                default: return new StudentDiscount();
+            }
+        }
+    }
+    class MovieTicket
+    {
+        private double price;
+        private Discount discount;
+        public void setPrice(double price)
+        {
+            this.price = price;
+        }
+        public void setDiscount(Discount discount)
+        {
+            this.discount = discount;
+        }
+        public double getPrice()
+        {
+            return discount.calculate(this.price);
+        }
+    }
+    interface Discount
+    {
+        double calculate(double price);
+    }
+    class StudentDiscount:Discount
+    {
+        public double calculate(double price)
+        {
+            Console.WriteLine("学生票：");
+            return price * 0.8;
+        }
+    }
+    class ChiDiscount : Discount
+    {
+        public double calculate(double price)
+        {
+            Console.WriteLine("儿童票：");
+            return price - 10;
+        }
+    }
+    class VIPDiscount : Discount
+    {
+        public double calculate(double price)
+        {
+            Console.WriteLine("VIP票：");
+            Console.WriteLine("增加积分");
+            return price * 0.5;
+        }
+    }
+}
